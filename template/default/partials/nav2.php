a0e8ee2e3aed39dc59c433208b722863162804a8
Miretazam Ciptaprima <Mcrealdeviant@gmail.com>
2020-05-23 14:24:52 +0700

    Navigation menu separated form index file

---
+++ template/default/partials/nav2.php
@@ -0,0 +1,51 @@
+<div class="container position-relative mt-5">
+    <nav id="menu" class="navbar navbar-expand-lg">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto font-weight-light centeri" style="margin-left:20px;">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Beranda <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item dropdown dropdown-slide dropdown-hover">
+            <a class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Profile
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a class="dropdown-item" href="index.php?p=history"><?php echo __('History'); ?></a>
+              <a class="dropdown-item" href="index.php?p=visi"><?php echo __('Vision and Mission'); ?></a>
+              <a class="dropdown-item" href="index.php?p=struktur_organisasi"><?php echo __('Organization Structure'); ?></a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Fasilitas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?p=news"><?php echo __('News'); ?></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?p=catalog"><?php echo __('Catalog'); ?></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?p=digital_collection"><?php echo __('Digital Collection'); ?></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?p=jurnal_akreditasi"><?php echo __('Journal of Accreditation'); ?></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?p=prosiding"><?php echo __('Proceedings'); ?></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?p=kuesioner"><?php echo __('Questionnaire'); ?></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><?php echo __('Download'); ?></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?p=laporan_akhir"><?php echo __('Submitting Final Report'); ?></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php?p=contact"><?php echo __('Contact'); ?></a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </div>
