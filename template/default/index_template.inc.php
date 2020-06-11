f363dee4541d7a6a111cd8e9da8447cdacb7d482
Miretazam Ciptaprima <Mcrealdeviant@gmail.com>
2020-06-11 23:51:50 +0700

    Updated weblink links
    Same as source site perpustakaan.pnj.ac.id

--- template/default/index_template.inc.php
+++ template/default/index_template.inc.php
@@ -319,63 +319,66 @@ include "partials/nav2.php";
         <div class="col-sm weblink-col mb-4">
           <h2>Tautan Terkait</h2>
           <ul>
-            <li><a href="https://google.com">Akademik & Kemahasiswaan</a></li>
-            <li><a href="https://google.com">AKN Demak</a></li>
-            <li><a href="https://google.com">Bidang Kerjasama</a></li>
-            <li><a href="https://google.com">IBIKK-LSK</a></li>
-            <li><a href="https://google.com">International Office</a></li>
-            <li><a href="https://google.com">Kearsipan</a></li>
-            <li><a href="https://google.com">Kemahasiswaan</a></li>
-            <li><a href="https://google.com">Keuangan</a></li>
-            <li><a href="https://google.com">Koperasi Karyawan</a></li>
-            <li><a href="https://google.com">LPSE</a></li>
-            <li><a href="https://google.com">LSP</a></li>
-            <li><a href="https://google.com">P3AI</a></li>
-            <li><a href="https://google.com">P3M</a></li>
-            <li><a href="https://google.com">Perencanaan</a></li>
-            <li><a href="https://google.com">Perpustakaan</a></li>
-            <li><a href="https://google.com">PUSDATIN</a></li>
-            <li><a href="https://google.com">Puskominfo Alumni</a></li>
-            <li><a href="https://google.com">Senat</a></li>
-            <li><a href="https://google.com">SPMI</a></li>
-            <li><a href="https://google.com">UP2B</a></li>
-            <li><a href="https://google.com">PUTOL</a></li>
+            <li><a href="http://akademik.pnj.ac.id/">Akademik & Kemahasiswaan</a></li>
+            <li><a href="https://humas.pnj.ac.id/">Akademik & Kemahasiswaan</a></li>
+            <li><a href="http://akndemak.ac.id/">AKN Demak</a></li>
+            <li><a href="http://kerjasama.pnj.ac.id/">Bidang Kerjasama</a></li>
+            <li><a href="http://ibikk-lsk.pnj.ac.id/">IBIKK-LSK</a></li>
+            <li><a href="http://internationaloffice.pnj.ac.id/">International Office</a></li>
+            <li><a href="http://arsip.pnj.ac.id/">Kearsipan</a></li>
+            <li><a href="http://kemahasiswaan.pnj.ac.id/">Kemahasiswaan</a></li>
+            <li><a href="http://keuangan.pnj.ac.id/">Keuangan</a></li>
+            <li><a href="http://kopkar.pnj.ac.id/">Koperasi Karyawan</a></li>
+            <li><a href="http://lpse.pnj.ac.id/">LPSE</a></li>
+            <li><a href="http://lsp.pnj.ac.id/">LSP</a></li>
+            <li><a href="http://p3ai.pnj.ac.id/">P3AI</a></li>
+            <li><a href="http://perencanaan.pnj.ac.id/">Perencanaan</a></li>
+            <li><a href="http://perpustakaan.pnj.ac.id/">Perpustakaan</a></li>
+            <li><a href="http://pusdatin.pnj.ac.id/">PUSDATIN</a></li>
+            <li><a href="http://ikapunija.pnj.ac.id/">Puskominfo Alumni</a></li>
+            <li><a href="http://senat.pnj.ac.id/">Senat</a></li>
+            <li><a href="http://spmi.pnj.ac.id/">SPMI</a></li>
+            <li><a href="http://up2b.pnj.ac.id/">UP2B</a></li>
+            <li><a href="https://up2m.pnj.ac.id/">UP2M</a></li>
+            <li><a href="https://putoi.pnj.ac.id/">PUTOI</a></li>
+            <li><a href="https://layanan.pnj.ac.id/">Layanan</a></li>
           </ul>
         </div>
         <div class="col-sm weblink-col mb-4">
           <h2>Jurusan</h2>
             <ul>
-            <li><a href="https://google.com">Teknik Sipil</a></li>
-            <li><a href="https://google.com">Teknik Elektro</a></li>
-            <li><a href="https://google.com">TIK</a></li>
-            <li><a href="https://google.com">TGP</a></li>
-            <li><a href="https://google.com">Akuntansi</a></li>
-            <li><a href="https://google.com">Administrasi Niaga</a></li>
-            <li><a href="https://google.com">Manajemen Pemasaran</a></li>
-            <li><a href="https://google.com">Bispro</a></li>
+            <li><a href="http://sipil.pnj.ac.id/">Teknik Sipil</a></li>
+            <li><a href="http://elektro.pnj.ac.id/">Teknik Elektro</a></li>
+            <li><a href="http://mesin.pnj.ac.id/">Teknik Mesin</a></li>
+            <li><a href="http://tik.pnj.ac.id/">Teknik Informatika & Komputer</a></li>
+            <li><a href="http://grafika.pnj.ac.id/">Teknik Grafika & Penerbitan</a></li>
+            <li><a href="http://akuntansi.pnj.ac.id/">Akuntansi</a></li>
+            <li><a href="http://bisnis.pnj.ac.id/">Administrasi Niaga</a></li>
+            <li><a href="http://mpwnbk.pnj.ac.id/">Manajemen Pemasaran</a></li>
+            <li><a href="https://bispro.pnj.ac.id/">Bispro</a></li>
           </ul>
         </div>
         <div class="col-sm weblink-col mb-4">
           <h2>Portal Aplikasi</h2>
           <ul>
-            <li><a href="https://google.com">Edmodo</a></li>
-            <li><a href="https://google.com">Google Classrom</a></li>
-            <li><a href="https://google.com">OPM</a></li>
-            <li><a href="https://google.com">Simpeg</a></li>
-            <li><a href="https://google.com">Simkeu</a></li>
-            <li><a href="https://google.com">Sink</a></li>
-            <li><a href="https://google.com">SIKTI</a></li>
-            <li><a href="https://google.com">SIKS</a></li>
-            <li><a href="https://google.com">Sister</a></li>
+            <li><a href="http://elearning.pnj.ac.id/">Elearning</a></li>
+            <li><a href="https://classroom.google.com/">Google Classrom</a></li>
+            <li><a href="http://opac.perpustakaan.pnj.ac.id/">OPAC</a></li>
+            <li><a href="http://simpeg.pnj.ac.id/portalsdm">Simpeg</a></li>
+            <li><a href="http://simkeu.pnj.ac.id/portal">Simkeu</a></li>
+            <li><a href="http://siak.pnj.ac.id/">Siak</a></li>
+            <li><a href="https://sikd.pnj.ac.id/">SIKD</a></li>
+            <li><a href="https://siks.pnj.ac.id/pages/login.php">SIKS</a></li>
+            <li><a href="http://sister.pnj.ac.id/auth/login">Sister</a></li>
           </ul>
         </div>
         <div class="col-sm weblink-col mb-4">
           <h2>Mahasiswa</h2>
           <ul>
-            <li><a href="https://google.com">Website Mahasiswa</a></li>
-            <li><a href="https://google.com">Email Mahasiswa</a></li>
-            <li><a href="https://google.com">Badan Eksekutif Mahasiswa</a></li>
-            <li><a href="https://google.com">Hima</a></li>
+            <li><a href="">Website Mahasiswa</a></li>
+            <li><a href="">Email Mahasiswa</a></li>
+            <li><a href="http://bem.pnj.ac.id/">Badan Eksekutif Mahasiswa</a></li>
+            <li><a href="">Hima</a></li>
           </ul>
         </div>
       </div>
