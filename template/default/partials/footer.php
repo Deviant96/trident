948f1a30107111d8fa141d54005bacda6ce4cd79
Miretazam Ciptaprima <Mcrealdeviant@gmail.com>
2020-07-27 11:06:37 +0700

    Weblink moved to footer.php file

---
+++ template/default/partials/footer.php
@@ -0,0 +1,77 @@
+<div id="weblink" class="pt-3 pb-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm weblink-col mb-4">
+        <h2><?php echo __('Related Links');  ?></h2>
+        <ul>
+          <li><a href="http://akademik.pnj.ac.id/">Akademik & Kemahasiswaan</a></li>
+          <li><a href="https://humas.pnj.ac.id/">Humas</a></li>
+          <li><a href="http://akndemak.ac.id/">AKN Demak</a></li>
+          <li><a href="http://kerjasama.pnj.ac.id/">Bidang Kerjasama</a></li>
+          <li><a href="http://ibikk-lsk.pnj.ac.id/">IBIKK-LSK</a></li>
+          <li><a href="http://internationaloffice.pnj.ac.id/">International Office</a></li>
+          <li><a href="http://arsip.pnj.ac.id/">Kearsipan</a></li>
+          <li><a href="http://kemahasiswaan.pnj.ac.id/">Kemahasiswaan</a></li>
+          <li><a href="http://keuangan.pnj.ac.id/">Keuangan</a></li>
+          <li><a href="http://kopkar.pnj.ac.id/">Koperasi Karyawan</a></li>
+          <li><a href="http://lpse.pnj.ac.id/">LPSE</a></li>
+          <li><a href="http://lsp.pnj.ac.id/">LSP</a></li>
+          <li><a href="http://p3ai.pnj.ac.id/">P3AI</a></li>
+          <li><a href="http://perencanaan.pnj.ac.id/">Perencanaan</a></li>
+          <li><a href="http://perpustakaan.pnj.ac.id/">Perpustakaan</a></li>
+          <li><a href="http://pusdatin.pnj.ac.id/">PUSDATIN</a></li>
+          <li><a href="http://ikapunija.pnj.ac.id/">Puskominfo Alumni</a></li>
+          <li><a href="http://senat.pnj.ac.id/">Senat</a></li>
+          <li><a href="http://spmi.pnj.ac.id/">SPMI</a></li>
+          <li><a href="http://up2b.pnj.ac.id/">UP2B</a></li>
+          <li><a href="https://up2m.pnj.ac.id/">UP2M</a></li>
+          <li><a href="https://putoi.pnj.ac.id/">PUTOI</a></li>
+          <li><a href="https://layanan.pnj.ac.id/">Layanan</a></li>
+        </ul>
+      </div>
+      <div class="col-sm weblink-col mb-4">
+        <h2><?php echo __('Majors');  ?></h2>
+          <ul>
+          <li><a href="http://sipil.pnj.ac.id/">Teknik Sipil</a></li>
+          <li><a href="http://elektro.pnj.ac.id/">Teknik Elektro</a></li>
+          <li><a href="http://mesin.pnj.ac.id/">Teknik Mesin</a></li>
+          <li><a href="http://tik.pnj.ac.id/">Teknik Informatika & Komputer</a></li>
+          <li><a href="http://grafika.pnj.ac.id/">Teknik Grafika & Penerbitan</a></li>
+          <li><a href="http://akuntansi.pnj.ac.id/">Akuntansi</a></li>
+          <li><a href="http://bisnis.pnj.ac.id/">Administrasi Niaga</a></li>
+          <li><a href="http://mpwnbk.pnj.ac.id/">Manajemen Pemasaran</a></li>
+          <li><a href="https://bispro.pnj.ac.id/">Bispro</a></li>
+        </ul>
+      </div>
+      <div class="col-sm weblink-col mb-4">
+        <h2><?php echo __('Application Portal');  ?></h2>
+        <ul>
+          <li><a href="http://elearning.pnj.ac.id/">Elearning</a></li>
+          <li><a href="https://classroom.google.com/">Google Classrom</a></li>
+          <li><a href="http://opac.perpustakaan.pnj.ac.id/">OPAC</a></li>
+          <li><a href="http://simpeg.pnj.ac.id/portalsdm">Simpeg</a></li>
+          <li><a href="http://simkeu.pnj.ac.id/portal">Simkeu</a></li>
+          <li><a href="http://siak.pnj.ac.id/">Siak</a></li>
+          <li><a href="https://sikd.pnj.ac.id/">SIKD</a></li>
+          <li><a href="https://siks.pnj.ac.id/pages/login.php">SIKS</a></li>
+          <li><a href="http://sister.pnj.ac.id/auth/login">Sister</a></li>
+        </ul>
+      </div>
+      <div class="col-sm weblink-col mb-4">
+        <h2><?php echo __('Student');  ?></h2>
+        <ul>
+          <li><a href="">Website Mahasiswa</a></li>
+          <li><a href="">Email Mahasiswa</a></li>
+          <li><a href="http://bem.pnj.ac.id/">Badan Eksekutif Mahasiswa</a></li>
+          <li><a href="">Hima</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+<footer class="bg-light">
+  <div class="container pt-3 pb-2">
+    <p class="blockquote-footer text-center text-dark">Copyright 2020 &copy; Perpustakaan Politeknik Negeri Jakarta</p>
+  </div>
+</footer>
