aa104e7349466906f7489727be6defc821387321
Miretazam Ciptaprima <Mcrealdeviant@gmail.com>
2020-07-27 11:15:21 +0700

    Update header.php

--- template/default/partials/header.php
+++ template/default/partials/header.php
@@ -3,22 +3,22 @@
     <div class="col">
     <div class="flags float-right">
       <ul>
-        <li><span class="small text-dark">Select Language/Pilih Bahasa : </span></li>
-        <li><a href="index.php?select_lang=id_ID"><img src="http://cdn2.iconfinder.com/data/icons/flags/flags/32/Indonesia.png" /></a></li>
+        <li><span class="small text-dark">Pilih Bahasa / Select Language : </span></li>
+        <li><a href="index.php?select_lang=id_ID"><img src="https://cdn2.iconfinder.com/data/icons/flags/flags/32/Indonesia.png" /></a></li>
         <li><a href="index.php?select_lang=en_US"><img src="https://cdn2.iconfinder.com/data/icons/fourth-of-july-13/32/flag_usa_america_united-32.png" /></a></li>
       </ul>
     </div>
     </div>
   </div>
   <div class="row pt-4">
-    <div class="col-lg-6">
+    <div class="col-lg-7">
       <a href="index.php" class="s-brand text-dark">
         <img class="s-logo animated flipInY delay7" src="<?php echo $sysconf['template']['dir']; ?>/default/img/logo.png" alt="<?php echo $sysconf['library_name']; ?>" />
         <h1 class="animated fadeInUp delay2"><?php echo $sysconf['library_name']; ?></h1>
         <div class="s-brand-tagline animated fadeInUp delay3"><?php echo $sysconf['library_subname']; ?></div>
       </a>
     </div>
-    <div class="col-lg-4">
+    <div class="col-lg-3">
       <div class="dv-header-search">
         <form action="index.php" method="get" autocomplete="off">
           <div class="marquee down">
@@ -34,7 +34,6 @@
               <button type="submit" name="search" value="search" style="display:none;"></button>
           </div>
         </form>
-        <a href="#" class="s-search-advances" width="800" height="500" title="<?php echo __('Advanced Search') ?>"><?php echo __('Advanced Search') ?></a>
       </div>
     </div>
     <div class="col-lg-2">
@@ -42,7 +41,7 @@
         ============================================= -->
     <?php echo (utility::isMemberLogin()) ? $header_info : 
     '<a href="index.php?p=member" class="btn login-btn">'.__('Member Area').'</a>
-    <div class="register-btn">Don\'t have an account? <a href="index.php?p=form">'.__('Register').'</a></div>'
+    <div class="register-btn">'.__('Don\'t have an account? <br>').'<a href="index.php?p=form">'.__('Register').'</a> | <a href="index.php?p=forgot_password">'.__('Forgot Password').'</a></div>'
     ; ?>
     </div>
   </div>
